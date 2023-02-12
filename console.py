c3a26d1e8826a3147c8e60a842533bcafa1eaeae
leroysb <leroybuliro@gmail.com>
2023-02-12 19:12:54 +0300

    set up entry and exit points

--- console.py
+++ console.py
@@ -6,13 +6,19 @@ import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    prompt = 'hbnb'
+    prompt = '(hbnb) '
 
     def do_quit(self, line):
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, line):
+        """Exit the program"""
         return True
+
+    def emptyline(self):
+        """Overwriting the emptyline method"""
+        return False
 
 
 if __name__ == '__main__':
